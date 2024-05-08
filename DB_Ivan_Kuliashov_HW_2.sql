bf1ff97a6b788c8a149cea622183e0e68dbb3609
IVAN KULIASHOV <42475953+Krivetka@users.noreply.github.com>
2024-05-08 16:59:18 +0200

    Update DB_Ivan_Kuliashov_HW_2.sql

--- DB_Ivan_Kuliashov_HW_2.sql
+++ DB_Ivan_Kuliashov_HW_2.sql
@@ -1,1 +1,19 @@
+CREATE TABLE Orders (
+    o_id SERIAL PRIMARY KEY,
+    order_date DATE NOT NULL
+);
+
+CREATE TABLE Products (
+    p_name TEXT PRIMARY KEY,
+    price MONEY NOT NULL
+);
+
+CREATE TABLE Order_Items (
+    order_id INT NOT NULL,
+    product_name TEXT NOT NULL,
+    amount NUMERIC(7,2) NOT NULL DEFAULT 1 CHECK (amount > 0),
+    PRIMARY KEY (order_id, product_name),
+    FOREIGN KEY (order_id) REFERENCES Orders(o_id),
+    FOREIGN KEY (product_name) REFERENCES Products(p_name)
+);
 
